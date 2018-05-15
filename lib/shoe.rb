768e4d08f8f7addbb099ab598bbd12040ed6e86e
Emily Welsch <emwelsch@gmail.com>
2018-05-15 21:56:16 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     # need something here so that brands are only counted when unique, like .uniq
-    BRANDS << brand
+    BRANDS << brand.uniq
   end
 
   def cobble
