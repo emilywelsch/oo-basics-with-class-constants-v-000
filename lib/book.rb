52146104a32ba41dcfe29a3962ec021c6fe175be
Emily Welsch <emwelsch@gmail.com>
2018-05-15 21:21:54 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 # learn spec/01_book_spec.rb
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   GENRES = []
 
@@ -12,5 +12,9 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
 
 end
